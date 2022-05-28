32dadc9e8d190f61e82b739a1daa4af5bee0c5c9
vishnu <vishnupardha1040@gmail.com>
2022-05-28 10:20:18 +0530

    sadf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import NewsList from './components/NewsList';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
+      
       <NewsList />
     </div>
   );
--- src/components/Newsitem.jsx
+++ src/components/Newsitem.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import './newsitem.css'
+
+
 function Newsitem({ title, description, url, urlToImage }) {
   return (
+      <div>
+          <div>
+              
+          </div>
     <div className="news-item">
       <img className='news-img' src={urlToImage} alt="New Image" />
       <h3>
         <a href={url}>{title}</a>
       </h3>
       <p>{description}</p>
+  
+    </div>
     </div>
   );
 }
